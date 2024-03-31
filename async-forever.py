84b90e2e60e73e136a515005d00aa87181d34546
tylerwarre <tyler@warrens.one>
2024-03-30 21:40:27 -0600

    re-wrote async socket to use queues to hold conns

--- async-forever.py
+++ async-forever.py
@@ -1,7 +1,7 @@
 import multiprocessing
-from re import sub
-import socketserver
-import subprocess
+from os import unlink
+from datetime import datetime, timedelta
+import socket
 
 # Create a semaphore that is initalized in each timer
 # daemon functions requires a lock on all semaphores
@@ -9,47 +9,72 @@ import subprocess
 # Add a handle_cmd() function to each timer that calls the appropriate command's method with multiprocessing.Process and set the approrpriate timerouts
 # Create a chart for each function whether it should be blocking and a reasonable timeout
 
-s_timer = multiprocessing.Semaphore()
+SOCK_PATH = "/tmp/my.socket"
+commands = multiprocessing.Queue()
+s_jira = multiprocessing.Semaphore(1)
 
-def worker(cmd, sock):
-    if cmd == "test":
-        thread = multiprocessing.Process(target=tmux, args=[cmd], daemon=True, name=cmd)
-        thread.start()
-    else:
-        thread = multiprocessing.Process(target=jira, args=[cmd], daemon=True, name=cmd)
-        thread.start()
-        thread.join()
+def handle(conn):
+    data = conn.recv(1024)
 
-        if thread.exitcode != 0:
-            print(f"Unable to complete {cmd}")
-            thread.terminate()
-            s_timer.release()
+    if not data:
+        conn.close()
+        return
 
-    sock.request.sendall(cmd.encode())
-    multiprocessing.active_children()
-
-def tmux(cmd):
-    s_timer.acquire(block=True)
-    subprocess.run(["tmux", "display-popup", "hello world"])
-    print(f"Tmux: {cmd}")
-    s_timer.release()
+    data = data.decode().rstrip()
+    commands.put(data)
+    conn.shutdown(socket.SHUT_RDWR)
+    conn.close()
+    return 0
 
 def jira(cmd):
-    s_timer.acquire(block=True)
-    print(f"Jira: {cmd}")
-    s_timer.release()
+    done = datetime.now() + timedelta(seconds=10)
+    while datetime.now() < done:
+        continue
+    print(cmd)
 
-class ThreadedUnixRequestHandler(socketserver.BaseRequestHandler):
-    def handle(self):
-        data = str(self.request.recv(1024), 'ascii')
-        worker(data, self)
+    return 0
 
-if __name__ == "__main__":
-    HOST, PORT = "localhost", 9999
-    #threading.Thread(target=worker, daemon=True).start()
+def tmux(cmd):
+    print(cmd)
+    return 0
 
-    # Create the server, binding to localhost on port 9999
-    with socketserver.ThreadingUnixStreamServer("/tmp/my.socket", ThreadedUnixRequestHandler) as server:
-        # Activate the server; this will keep running until you
-        # interrupt the program with Ctrl-C
-        server.serve_forever()
+def process_cmds():
+    while True:
+        multiprocessing.active_children()
+        cmd = commands.get()
+        print(f"processing: {cmd}")
+
+        if cmd == "jira":
+            s_jira.acquire()
+            task = multiprocessing.Process(target=jira, args=[cmd,], daemon=True)
+            task.start()
+            task.join(timeout=15)
+            if task.is_alive():
+                print(f"Unable to process {cmd}")
+                task.terminate()
+                s_jira.release()
+            else:
+                s_jira.release()
+        elif cmd == "tmux":
+            s_jira.acquire()
+            task = multiprocessing.Process(target=tmux, args=[cmd,], daemon=True)
+            task.start()
+            task.join(timeout=3)
+            if task.is_alive():
+                print(f"Unable to process {cmd}")
+                task.terminate()
+                s_jira.release()
+            else:
+                s_jira.release()
+
+
+multiprocessing.Process(target=process_cmds).start()
+sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+unlink(SOCK_PATH)
+sock.bind(SOCK_PATH)
+sock.listen(4)
+
+while True:
+    multiprocessing.active_children()
+    conn, addr = sock.accept()
+    multiprocessing.Process(target=handle, args=[conn,], daemon=True).start()
